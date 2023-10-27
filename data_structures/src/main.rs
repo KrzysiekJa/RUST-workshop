19712b65eeb6568b530d8d54c0cee281f25576eb
KrzysiekJa <kjmj77@gmail.com>
2023-10-27 13:20:15 +0200

    Added exer 1 in data_structures/

--- data_structures/src/main.rs
+++ data_structures/src/main.rs
@@ -1,8 +1,18 @@
-use std::collections::LinkedList;
+use std::collections::{LinkedList};
 mod utils;
 
-fn main() {
+fn main()
+{
+    //exercise 1
+    println!(">> 1");
+    let linked_list: LinkedList<i32> = LinkedList::from([7, 1, 291, 15, -2, 3, 55, 102]);
+    for i in linked_list.iter() {
+        print!("-> {} ", i);
+    }
+    let results_minimum = utils::min_search_list(linked_list);
+    println!("\n min: {}, O(n): {}", results_minimum.0, results_minimum.1);
 
-    let list = LinkedList::from([7, 1, 291, 15, -2, 3, 55, 102]);
+    //exercise 2
+    println!(">> 2");
 
 }
--- data_structures/src/utils.rs
+++ data_structures/src/utils.rs
@@ -1,17 +1,19 @@
 use std::collections::LinkedList;
 
-pub fn min_search_list(list: LinkedList<i32>) -> i32
+pub fn min_search_list(list: LinkedList<i32>) -> (i32, u32)
 {
-    let mut amount = list.len() - 1;
-    let mut iter = list.iter();
-    let mut min = iter.next();
+    let mut amount: usize = list.len() - 1;
+    let mut counted: u32 = 0;
+    let mut iter: std::collections::linked_list::Iter<'_, i32> = list.iter();
+    let mut min: Option<&i32> = iter.next();
 
     while amount > 0 {
         let tmp = iter.next();
         if min > tmp {
             min = tmp;
         }
+        counted += 1;
         amount -= 1;
     }
-    return * min.unwrap();
+    return (* min.unwrap(), counted);
 }
