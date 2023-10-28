8cfa25405c216b92603564250302ae0cb7a4d3ee
KrzysiekJa <kjmj77@gmail.com>
2023-10-28 13:18:39 +0200

    Added exer 3 in data_structures/

--- data_structures/src/main.rs
+++ data_structures/src/main.rs
@@ -1,18 +1,24 @@
-use std::collections::{LinkedList};
+use std::collections::LinkedList;
 mod utils;
 
 fn main()
 {
     //exercise 1
     println!(">> 1");
-    let linked_list: LinkedList<i32> = LinkedList::from([7, 1, 291, 15, -2, 3, 55, 102]);
+    let mut linked_list: LinkedList<i32> = LinkedList::from([7, 1, 291, 15, -2, 3, 55, 102]);
     for i in linked_list.iter() {
         print!("-> {} ", i);
     }
-    let results_minimum = utils::min_search_list(linked_list);
-    println!("\n min: {}, O(n): {}", results_minimum.0, results_minimum.1);
+    let results_minimum: (i32, u32) = utils::min_search_list(linked_list);
+    println!("\n min: {}, comparisons: {}", results_minimum.0, results_minimum.1);
 
-    //exercise 2
-    println!(">> 2");
+    //exercise 3
+    println!(">> 3");
+    linked_list = LinkedList::from([7, 1, 291, 15, -2, 3, 55, 102, -2, 291, 34]);
+    for i in linked_list.iter() {
+        print!("-> {} ", i);
+    }
+    let results_minimum: (i32, u32, u32) = utils::min_idx_search_list(linked_list);
+    println!("\n min: {}, index: {}, comparisons: {}", results_minimum.0, results_minimum.1, results_minimum.2);
 
 }
