52fb1db12e0a73ceae343ccc8881bbab6d3923fe
KrzysiekJa <kjmj77@gmail.com>
2023-11-30 22:18:18 +0100

    Added Pear obj in main in structures/

--- structures/src/main.rs
+++ structures/src/main.rs
@@ -2,5 +2,9 @@ mod pear;
 
 fn main()
 {
-    println!("Hello, world!");
+    let color: pear::Color = pear::Color::Orange;
+    let taste: pear::Taste = pear::Taste::Dry;
+    let pear: pear::Pear = pear::Pear::new("Anjou Anjou".to_string(), color, 0.3, 0.2, taste);
+
+    println!("pear: {:?}", pear);
 }
