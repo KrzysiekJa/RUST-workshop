6a1daa3a8d68a9c5a7d6c6cdec0370e0e258fc55
KrzysiekJa <kjmj77@gmail.com>
2023-12-02 23:42:01 +0100

    Added Pear methods in main in structures/

--- structures/src/main.rs
+++ structures/src/main.rs
@@ -4,7 +4,24 @@ fn main()
 {
     let color: pear::Color = pear::Color::Orange;
     let taste: pear::Taste = pear::Taste::Dry;
-    let pear: pear::Pear = pear::Pear::new("Anjou Anjou".to_string(), color, 0.3, 0.2, taste);
+    let mut pear: pear::Pear = pear::Pear::new("Anjou Anjou".to_string(), color, 0.3, 0.2, taste);
+
+    println!("pear: {:?}", pear);
+
+    pear.get_variety();
+    *pear.set_variety() = "Papierówka".to_string();
+
+    pear.get_color();
+    *pear.set_color() = pear::Color::Green;
+
+    pear.get_weight();
+    *pear.set_weight() = 0.15;
+
+    pear.get_size();
+    *pear.set_size() = 0.1;
+
+    pear.get_taste();
+    *pear.set_taste() = pear::Taste::Sweet;
 
     println!("pear: {:?}", pear);
 }
